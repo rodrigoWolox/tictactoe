726f214cbdb83d56b8639c733827a6269894dd5d
rodrigoWolox <rodrigo.hernandez@wolox.com.ar>
2018-10-24 17:23:23 -0300

    square in clickeable

--- rh-tictactoe/src/app/screens/Game/components/Board/components/Square/index.js
+++ rh-tictactoe/src/app/screens/Game/components/Board/components/Square/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
+        {/* TODO */}
+      </button>
+    );
   }
 }
 
--- rh-tictactoe/src/app/screens/Game/index.js
+++ rh-tictactoe/src/app/screens/Game/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Board from './Board';
+import Board from './components/Board';
 
 class Game extends React.Component {
   render() {
