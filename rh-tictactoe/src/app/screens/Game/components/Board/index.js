36645b67b65af3074e74bfbc740c2d3a077495d4
rodrigoWolox <rodrigo.hernandez@wolox.com.ar>
2018-10-26 16:05:58 -0300

    Now the tictactoe declares a winner, when there is one.

--- rh-tictactoe/src/app/screens/Game/components/Board/index.js
+++ rh-tictactoe/src/app/screens/Game/components/Board/index.js
@@ -7,20 +7,32 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
   handleClick = i => {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares });
+    if (calculateWinner(squares) === null && squares[i] === null) {
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares,
+        xIsNext: !this.state.xIsNext
+      });
+    }
   };
 
   renderSquare = i => <Square position={i} value={this.state.squares[i]} onClick={this.handleClick} />;
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner){
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');;
+    }
 
     return (
       <React.Fragment>
@@ -46,3 +58,23 @@ class Board extends React.Component {
 }
 
 export default Board;
+
+function calculateWinner(squares) {
+  let winner = null;
+  const lines = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) winner = squares[a];
+  }
+  return winner;
+}
