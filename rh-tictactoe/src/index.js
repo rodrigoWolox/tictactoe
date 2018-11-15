617193e4e273c0c44f2590aec034a38dcda5580f
rodrigoWolox <rodrigo.hernandez@wolox.com.ar>
2018-11-15 14:42:32 -0300

    checking out

--- rh-tictactoe/src/index.js
+++ rh-tictactoe/src/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import { store } from './redux/store';
 import Game from './app/screens/Game';
 import Login from './app/screens/Login';
 import './scss/index.scss';
 
+const Links = () => (
+  <nav>
+    <Link to="/">Log Out</Link>
+    <Link to="/custom">Custom</Link>
+  </nav>
+);
+
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
         <Router>
+          <Links />
           <Switch>
             <Route exact path="/" component={Login} />
             <Route exact path="/game" component={Game} />
