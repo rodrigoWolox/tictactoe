c86e3de2b2d89b0bf0b39fdd13be767d0fbb8a5f
rodrigoWolox <rodrigo.hernandez@wolox.com.ar>
2018-11-15 17:33:26 -0300

    topbar desing ready

---
+++ rh-tictactoe/src/app/screens/Topbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import Styles from './styles.scss';
+
+const Links = () => (
+  <nav className={Styles.topbar}>
+    <NavLink className={Styles.link} to="/">
+      Log Out
+    </NavLink>
+    <NavLink className={Styles.link} to="/custom">
+      Custom
+    </NavLink>
+  </nav>
+);
+
+export default Links;
--- rh-tictactoe/src/index.js
+++ rh-tictactoe/src/index.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { store } from './redux/store';
 import Game from './app/screens/Game';
+import Topbar from './app/screens/Topbar';
 import Login from './app/screens/Login';
 import './scss/index.scss';
-
-const Links = () => (
-  <nav>
-    <Link to="/">Log Out</Link>
-    <Link to="/custom">Custom</Link>
-  </nav>
-);
 
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
         <Router>
-          <Links />
+          <Topbar />
           <Switch>
             <Route exact path="/" component={Login} />
             <Route exact path="/game" component={Game} />
