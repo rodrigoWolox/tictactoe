486b45276783e711b056c0c1373163e9c7c55676
rodrigoWolox <rodrigo.hernandez@wolox.com.ar>
2018-11-16 19:31:07 -0300

    will continue at home

--- rh-tictactoe/src/index.js
+++ rh-tictactoe/src/index.js
@@ -4,22 +4,19 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { store } from './redux/store';
-import Game from './app/screens/Game';
 import Login from './app/screens/Login';
-import Stats from './app/screens/Stats';
-import Topbar from './app/screens/Topbar';
+import App from './app/components/Routes/App';
+import PrivateRoute from './app/components/Routes/PrivateRoute';
 import './scss/index.scss';
 
-class App extends React.Component {
+class AppContainer extends React.Component {
   render() {
     return (
       <Provider store={store}>
         <Router>
-          <Topbar />
           <Switch>
             <Route exact path="/" component={Login} />
-            <Route path="/game" component={Game} />
-            <Route path="/stats" component={Stats} />
+            <PrivateRoute path="/app" component={App} />
           </Switch>
         </Router>
       </Provider>
@@ -27,4 +24,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppContainer />, document.getElementById('root'));
