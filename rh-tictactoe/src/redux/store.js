1d69f2ae782cf0bca9dc484ade02289294cd5e31
rodrigoWolox <rodrigo.hernandez@wolox.com.ar>
2018-11-20 14:21:24 -0300

    There was a reducer for the topbar that wasnt neccesary so it was deleted and removed from the store.

--- rh-tictactoe/src/redux/store.js
+++ rh-tictactoe/src/redux/store.js
@@ -4,13 +4,11 @@ import { reducer as formReducer } from 'redux-form';
 
 import game from './game/reducer';
 import login from './login/reducer';
-import topbar from './topbar/reducer';
 
 const reducers = combineReducers({
   form: formReducer,
   game,
-  login,
-  topbar
+  login
 });
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
